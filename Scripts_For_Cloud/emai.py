975232401e858ca0cea4bd990e2233af49f97a57
Rohan Chougule <rohan183chougule@gmail.com>
2018-04-15 03:37:57 +0530

    Update emai.py

--- Scripts_For_Cloud/emai.py
+++ Scripts_For_Cloud/emai.py
@@ -1,3 +1,3 @@
 import yagmail
-yag = yagmail.SMTP("rohan183chougule@gmail.com", 'Viewsoniccorei318158114Vi7')
-yag.send("rohan183chougule@gmail.com", "Baby", "is Screaming")
+yag = yagmail.SMTP("email", 'password')
+yag.send("emailToId", "Baby", "is Screaming")
